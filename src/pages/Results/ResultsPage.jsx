e8f0242da56d1a43fd2ce718ad937eeed9deba4b
Mighty Eagle <mightyeaglecorp@gmail.com>
2022-03-26 21:00:23 +0100

    made changes

--- src/pages/Results/ResultsPage.jsx
+++ src/pages/Results/ResultsPage.jsx
@@ -1,23 +1,60 @@
 import { Avatar, Typography } from "@mui/material";
 import React, { useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Card } from "react-bootstrap";
+
 import { result } from "../../services/services";
 
 export default function ResultsPage() {
-  const [data, setResult] = useState(result);
+  const [data] = useState(result);
+
   return (
     <div>
       <Container>
         <div className="border border-dark p-3 rounded-3 mt-3">
-          <div className="d-flex justify-content-end">
-            <Avatar src={data.imageUrl} sx={{ width: 100, height: 100 }} />
+          <div className="d-flex justify-content-center mb-3">
+            <Avatar
+              src={data.imageUrl}
+              sx={{ width: 100, height: 100 }}
+              variant="rounded"
+            />
           </div>
-          <div>
-            <Typography>First Name: {result.firstName}</Typography>
-          </div>
-          <div>
-            <Typography>Last Name: {result.lastName}</Typography>
-          </div>
+          <Typography className="text-center mb-2">{`${data.firstName} ${data.lastName}`}</Typography>
+          <Typography className="text-center mb-2">{`${data.regNumber}`}</Typography>
+          <Typography className="text-center mb-2">{`${data.institution.name}`}</Typography>
+          <Typography className="text-center mb-4">{`${data.examinationBody.name}`}</Typography>
+
+          <Row>
+            <div className="col-md-4 mb-2">
+              <Card>
+                <Card.Body>
+                  <Card.Title>{data.subject1.subject.title}</Card.Title>
+                  <Card.Text>
+                    <Typography>{data.subject1.grade}</Typography>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-md-4 mb-2">
+              <Card>
+                <Card.Body>
+                  <Card.Title>{data.subject2.subject.title}</Card.Title>
+                  <Card.Text>
+                    <Typography>{data.subject2.grade}</Typography>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-md-4 mb-2">
+              <Card>
+                <Card.Body>
+                  <Card.Title>{data.subject3.subject.title}</Card.Title>
+                  <Card.Text>
+                    <Typography>{data.subject3.grade}</Typography>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </Row>
         </div>
       </Container>
     </div>
