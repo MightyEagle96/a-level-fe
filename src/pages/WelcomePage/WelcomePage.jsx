342bd173167057509ccfc9441a088a34588d0e0d
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-03-28 22:35:54 +0100

    made changes

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,48 +1,126 @@
 import { Button, TextField, Typography } from "@mui/material";
 import React from "react";
-import img1 from "../../images/img1.jpg";
+import { Container } from "react-bootstrap";
+
+import "./WelcomePage.css";
+import { green } from "@mui/material/colors";
 
 export default function WelcomePage() {
   return (
     <div>
       <div className="mt-5">
-        <div className="container shadow-lg rounded-3 p-3">
-          <div className="row">
-            <div className="col-md-6">
-              <img
-                src={img1}
-                alt="img1"
-                className="img-fluid shadow-lg rounded-3"
-              />
-            </div>
-            <div className="col-md-6 border-start">
-              <div className="mt-3">
-                <Typography variant="h4" color="CaptionText" gutterBottom>
-                  A' LEVEL VERIFICATION SYSTEM
-                </Typography>
-              </div>
-              <div className="mt-2">
-                <TextField
-                  type="text"
-                  label="ABN"
-                  helperText="Enter your A Level Bank number on your scratch card"
-                />
-              </div>
-              <div className="mt-2">
-                <TextField
-                  type="text"
-                  label="Registration Number"
-                  helperText="Enter your registration Number"
-                />
-              </div>
-              <div className="mt-2">
-                <Button variant="contained" color="secondary">
-                  Check Result
-                </Button>
+        <div className="container d-none d-lg-block">
+          <div className="row mdMobileBanner" style={{ height: 400 }}>
+            <div className="col-md-6"></div>
+            <div className="col-md-6 d-flex align-items-center justify-content-center">
+              <div className="mdBanner p-5">
+                <div className="">
+                  <Typography
+                    variant="h5"
+                    color={green[500]}
+                    letterSpacing={3}
+                    gutterBottom
+                    align="center"
+                  >
+                    A' LEVEL VERIFICATION SYSTEM
+                  </Typography>
+                </div>
+                <div className="mt-2 d-flex justify-content-center">
+                  <TextField
+                    type="text"
+                    label="ABN"
+                    helperText="Enter your A Level Bank number on your scratch card"
+                  />
+                </div>
+                <div className="mt-2 d-flex justify-content-center">
+                  <TextField
+                    type="text"
+                    label="Registration Number"
+                    helperText="Enter your registration Number"
+                  />
+                </div>
+                <div className="mt-2 d-flex justify-content-center">
+                  <Button variant="contained" color="success">
+                    Check Result
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="container d-none d-md-block d-lg-none">
+          <div className="row mdMobileBanner ">
+            <div className="col-md-6"></div>
+            <div className="col-md-6 mdBanner p-4">
+              <div>
+                <div className="mt-3">
+                  <Typography
+                    variant="h5"
+                    color={green[500]}
+                    letterSpacing={3}
+                    gutterBottom
+                    align="center"
+                  >
+                    A' LEVEL VERIFICATION SYSTEM
+                  </Typography>
+                </div>
+                <div className="mt-2 d-flex justify-content-center">
+                  <TextField
+                    type="text"
+                    label="ABN"
+                    helperText="Enter your A Level Bank number on your scratch card"
+                  />
+                </div>
+                <div className="mt-2 d-flex justify-content-center">
+                  <TextField
+                    type="text"
+                    label="Registration Number"
+                    helperText="Enter your registration Number"
+                  />
+                </div>
+                <div className="mt-2 d-flex justify-content-center">
+                  <Button variant="contained" color="success">
+                    Check Result
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Container className="d-sm-block d-md-none">
+          <div className="mobileBanner p-5 shadow-lg rounded-3">
+            <div className="mt-3">
+              <Typography
+                variant="h5"
+                color={green[500]}
+                letterSpacing={3}
+                align="center"
+                gutterBottom
+              >
+                A' LEVEL VERIFICATION SYSTEM
+              </Typography>
+            </div>
+            <div className="mt-2 d-flex d-flex justify-content-center">
+              <TextField
+                type="text"
+                label="ABN"
+                helperText="Enter your A'Level Scratch Card PIN"
+              />
+            </div>
+            <div className="mt-2 d-flex d-flex justify-content-center">
+              <TextField
+                type="text"
+                label="Registration Number"
+                helperText="Enter your registration Number"
+              />
+            </div>
+            <div className="mt-4 d-flex d-flex justify-content-center">
+              <Button variant="contained" color="success">
+                Check Result
+              </Button>
+            </div>
+          </div>
+        </Container>
       </div>
     </div>
   );
