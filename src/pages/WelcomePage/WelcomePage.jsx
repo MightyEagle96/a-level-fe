e5610f2d63b4cf0bd8528156f4e16a236ce521a5
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-03-29 10:00:13 +0100

    made changes

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,9 @@
 import { Button, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-
 import "./WelcomePage.css";
 import { green } from "@mui/material/colors";
-
 import { Spinner } from "react-bootstrap";
-import img1 from "../../images/img1.jpg";
 import { httpService } from "../../services/services";
 
 export default function WelcomePage() {
@@ -34,13 +31,13 @@ export default function WelcomePage() {
   };
   return (
     <div>
-      <div className="mt-5">
+      <div className="mt-3">
         <div className="container d-none d-lg-block">
           <div className="row mdMobileBanner" style={{ height: 400 }}>
             <div className="col-md-6"></div>
             <div className="col-md-6 d-flex align-items-center justify-content-center">
               <div className="mdBanner p-5">
-                <div className="">
+                <div className=" mb-4">
                   <Typography
                     variant="h5"
                     color={green[500]}
@@ -51,24 +48,50 @@ export default function WelcomePage() {
                     A' LEVEL VERIFICATION SYSTEM
                   </Typography>
                 </div>
-                <div className="mt-2 d-flex justify-content-center">
-                  <TextField
-                    type="text"
-                    label="ABN"
-                    helperText="Enter your A Level Bank number on your scratch card"
-                  />
-                </div>
-                <div className="mt-2 d-flex justify-content-center">
-                  <TextField
-                    type="text"
-                    label="Registration Number"
-                    helperText="Enter your registration Number"
-                  />
-                </div>
-                <div className="mt-2 d-flex justify-content-center">
-                  <Button variant="contained" color="success">
-                    Check Result
-                  </Button>
+                <form onSubmit={UseScratchCard}>
+                  <div className="mt-2 d-flex justify-content-center">
+                    <TextField
+                      type="text"
+                      label="PIN"
+                      helperText="Enter your A Level Bank number on your scratch card"
+                      name="pin"
+                      value={data.pin}
+                      onChange={handlePinInput}
+                    />
+                  </div>
+                  <div className="mt-2 d-flex justify-content-center">
+                    <TextField
+                      type="text"
+                      label="Reg. Number"
+                      helperText="Enter your registration Number"
+                      name="regNumber"
+                      value={data.regNumber}
+                      onChange={handlePinInput}
+                    />
+                  </div>
+                  <div className="mt-2 d-flex justify-content-center">
+                    <Button
+                      variant="contained"
+                      color="success"
+                      className="me-2"
+                      type="submit"
+                    >
+                      Check Result
+                    </Button>
+                    {loading ? (
+                      <Spinner variant="success" animation="border" />
+                    ) : null}
+                  </div>
+                </form>
+                <div className="mt-3">
+                  <span className="">
+                    <Typography align="center" variant="body1">
+                      Don't have a pin?{" "}
+                      <Button style={{ color: green[500] }}>
+                        purchase one now
+                      </Button>
+                    </Typography>
+                  </span>
                 </div>
               </div>
             </div>
@@ -90,32 +113,58 @@ export default function WelcomePage() {
                     A' LEVEL VERIFICATION SYSTEM
                   </Typography>
                 </div>
-                <div className="mt-2 d-flex justify-content-center">
-                  <TextField
-                    type="text"
-                    label="ABN"
-                    helperText="Enter your A Level Bank number on your scratch card"
-                  />
-                </div>
-                <div className="mt-2 d-flex justify-content-center">
-                  <TextField
-                    type="text"
-                    label="Registration Number"
-                    helperText="Enter your registration Number"
-                  />
-                </div>
-                <div className="mt-2 d-flex justify-content-center">
-                  <Button variant="contained" color="success">
-                    Check Result
-                  </Button>
+                <form onSubmit={UseScratchCard}>
+                  <div className="mt-2 d-flex justify-content-center">
+                    <TextField
+                      type="text"
+                      label="PIN"
+                      helperText="Enter your A Level Bank number on your scratch card"
+                      name="pin"
+                      value={data.pin}
+                      onChange={handlePinInput}
+                    />
+                  </div>
+                  <div className="mt-2 d-flex justify-content-center">
+                    <TextField
+                      type="text"
+                      label="Reg. Number"
+                      helperText="Enter your registration Number"
+                      name="regNumber"
+                      value={data.regNumber}
+                      onChange={handlePinInput}
+                    />
+                  </div>
+                  <div className="mt-2 d-flex justify-content-center">
+                    <Button
+                      variant="contained"
+                      color="success"
+                      className="me-2"
+                      type="submit"
+                    >
+                      Check Result
+                    </Button>
+                    {loading ? (
+                      <Spinner variant="success" animation="border" />
+                    ) : null}
+                  </div>
+                </form>
+                <div className="mt-3">
+                  <span className="">
+                    <Typography align="center" variant="body1">
+                      Don't have a pin?{" "}
+                      <Button style={{ color: green[500] }}>
+                        purchase one now
+                      </Button>
+                    </Typography>
+                  </span>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <Container className="d-sm-block d-md-none">
-          <div className="mobileBanner p-5 shadow-lg rounded-3">
-            <div className="mt-3">
+          <div className="mobileBanner p-3 shadow-lg rounded-3">
+            <div className="mt-3 mb-4">
               <Typography
                 variant="h5"
                 color={green[500]}
@@ -126,24 +175,50 @@ export default function WelcomePage() {
                 A' LEVEL VERIFICATION SYSTEM
               </Typography>
             </div>
-            <div className="mt-2 d-flex d-flex justify-content-center">
-              <TextField
-                type="text"
-                label="ABN"
-                helperText="Enter your A'Level Scratch Card PIN"
-              />
-            </div>
-            <div className="mt-2 d-flex d-flex justify-content-center">
-              <TextField
-                type="text"
-                label="Registration Number"
-                helperText="Enter your registration Number"
-              />
-            </div>
-            <div className="mt-4 d-flex d-flex justify-content-center">
-              <Button variant="contained" color="success">
-                Check Result
-              </Button>
+            <form onSubmit={UseScratchCard}>
+              <div className="mb-4 d-flex d-flex justify-content-center">
+                <TextField
+                  type="text"
+                  label="PIN"
+                  helperText="Enter your A'Level Scratch Card PIN"
+                  name="pin"
+                  value={data.pin}
+                  onChange={handlePinInput}
+                />
+              </div>
+              <div className="mb-4 mt-2 d-flex d-flex justify-content-center">
+                <TextField
+                  type="text"
+                  label="Reg. Number"
+                  helperText="Enter your registration Number"
+                  name="regNumber"
+                  value={data.regNumber}
+                  onChange={handlePinInput}
+                />
+              </div>
+              <div className=" d-flex d-flex justify-content-center">
+                <Button
+                  variant="contained"
+                  color="success"
+                  className="me-2"
+                  type="submit"
+                >
+                  Check Result
+                </Button>
+                {loading ? (
+                  <Spinner variant="success" animation="border" />
+                ) : null}
+              </div>
+            </form>
+            <div className="mt-3">
+              <span className="">
+                <Typography align="center" variant="body2">
+                  Don't have a pin?{" "}
+                  <Button style={{ color: green[500] }}>
+                    purchase one now
+                  </Button>
+                </Typography>
+              </span>
             </div>
           </div>
         </Container>
