5d271695965386b19b6ee426af1d8459a929b7a6
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-03-29 23:08:36 +0100

    made changes

--- src/components/RegisteredInstitutions.jsx
+++ src/components/RegisteredInstitutions.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar, Button, Typography } from "@mui/material";
-import { Card } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
 import { School } from "@mui/icons-material";
 import { blueGrey } from "@mui/material/colors";
+import { httpService } from "../services/services";
 
 export default function RegisteredInstitutions() {
+  const [length, setLength] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const GetLength = async () => {
+    setLoading(true);
+    const res = await httpService.get("viewInstitutions");
+    setLength(res.data.length);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    GetLength();
+  }, []);
   return (
     <Card>
       <div className="d-flex justify-content-between p-3">
@@ -14,10 +27,17 @@ export default function RegisteredInstitutions() {
           </Typography>
           <School sx={{ color: blueGrey[500], fontWeight: 700 }} />
           <Button sx={{ color: blueGrey[500] }}>view</Button>
+          {loading ? (
+            <Spinner
+              animation="border"
+              size="sm"
+              style={{ color: blueGrey[300] }}
+            />
+          ) : null}
         </div>
         <div>
           <Avatar sx={{ bgcolor: blueGrey[500] }}>
-            <Typography>50</Typography>
+            <Typography>{length}</Typography>
           </Avatar>
         </div>
       </div>
